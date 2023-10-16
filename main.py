3e643d2ef936c61802506a7c8f0da19947a41b58
Lalith Kota <kotalalith@gmail.com>
2023-10-16 07:49:53 +0530

    Fixed Errors with ID FA Translation

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from gpb_simple_mpesa_payment_backend.app import (
     Initializer as SimpleMpesaPaymentBackendInitializer,
 )
 from gpb_translate_id_fa.app import Initializer as TranslateIdInitializer
+from openg2p_common_g2pconnect_id_mapper.app import (
+    Initializer as G2pConnectMapperInitializer,
+)
 
 main_init = CoreInitializer()
+G2pConnectMapperInitializer()
 TranslateIdInitializer()
-
 SimpleMpesaPaymentBackendInitializer()
 
 
