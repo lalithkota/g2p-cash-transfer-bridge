e65ac77706599a969cfa8024c29ec06865f3c72f
Lalith Kota <kotalalith@gmail.com>
2023-10-17 11:22:54 +0530

    Added ping for health check

--- main.py
+++ main.py
@@ -11,11 +11,13 @@ from gpb_translate_id_fa.app import Initializer as TranslateIdInitializer
 from openg2p_common_g2pconnect_id_mapper.app import (
     Initializer as G2pConnectMapperInitializer,
 )
+from openg2p_fastapi_common.ping import PingInitializer
 
 main_init = CoreInitializer()
 G2pConnectMapperInitializer()
 TranslateIdInitializer()
 SimpleMpesaPaymentBackendInitializer()
 MojaloopSdkBackendInitializer()
+PingInitializer()
 
 main_init.main()
